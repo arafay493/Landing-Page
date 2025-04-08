f5b01a6dc294ae46a84264266bba2b40c36da364
arafay493 <arafay493@gmail.com>
2025-04-08 10:34:55 +0500

    Slide Show responsive done.

--- components/SlideShow.jsx
+++ components/SlideShow.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Slider from "react-slick";
 
 const SlideShow = () => {
@@ -10,25 +10,45 @@ const SlideShow = () => {
     "/images/fedex.png",
     "/images/hubspot.png",
   ];
-  var settings = {
-    // dots: true,
+
+  const settings = {
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024, // large screens
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768, // tablets
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480, // mobile
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
+
   return (
-    <div className="slider-container my-10 py-20 border-gray-400 border-0 border-t-1 border-b-1">
+    <div className="slider-container my-10 py-10 px-4 sm:px-10 border-t border-b border-gray-200">
       <Slider {...settings}>
         {images.map((url, index) => (
           <div key={index} className="flex justify-center">
             <img
               src={url}
               alt={`Slide ${index + 1}`}
-              className="m-auto"
-            //   style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+              className="max-h-[80px] object-contain m-auto"
             />
           </div>
         ))}
